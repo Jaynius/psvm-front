8977cc9f6c083953da9992fbe3e2855cde253681
bram <bramweljumab@gmail.com>
2024-04-09 10:03:02 +0300

    added vehicle owner page and ew details

--- src/vehicleOwner/VehicleOwner.jsx
+++ src/vehicleOwner/VehicleOwner.jsx
@@ -3,16 +3,40 @@ function VehicleOwner(){
     return (
         <>
         <div className="vehicle-owner-container">
-            <div className="vehicles-container">
-                <h1>vehicle</h1>
+            <div className="header">
+                <div className="vehicles">
+                    <h2>vehicles: </h2>
+                <button classnName="header-link" >profile</button>
+                </div>
+                <div className="drivers">
+                    <h2>Drivers: </h2>
+                <button classnName="header-link" >vehicle operation</button>
+                </div>
+                <div className="conductors">
+                    <h2>Conductors:</h2>
+                <button classnName="header-link" >profile</button>
+
+                </div>
             </div>
-            <div className="drivers-container">
-                <h1>driver</h1>
+
+            <div className="body-container">
+                <div className="body-details">
+                    <h2>details</h2>
+
+                </div>
+                <div className="profile">
+                    <h2>profile</h2>
+                    <label htmlFor="">id number: </label>
+                    <label htmlFor=""> name:</label><br />
+                    <label htmlFor="">contact:</label><br />
+                    <label htmlFor="">email</label>
+                </div>
+                
+                
+                
+                
             </div>
-            <div className="conductors-container">
-                <h1>conductor</h1>
             </div>
-        </div>
         </>
     )
 }
